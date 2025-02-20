f5fc911a097ef9d0d5a25b6e2d555539fdb4b753
Balijepalli Vijaya Lalitha <balijepalliVijayalalitha@gmail.com>
2025-02-20 11:32:35 +0530

    Add files via upload

---
+++ backend/auth.py
@@ -0,0 +1,21 @@
+from passlib.context import CryptContext
+import jwt, datetime
+
+SECRET_KEY = "your_secret_key"
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_jwt_token(username: str):
+    payload = {
+        "sub": username,
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+    }
+    return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+
+def decode_jwt_token(token: str):
+    return jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
